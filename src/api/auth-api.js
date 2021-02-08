1491ef9d9b66ef41a582ef48b53a4b4078034dcc
Muhammed Kadıoğlu <36601122+MuhammedKadioglu@users.noreply.github.com>
2021-02-08 18:43:22 +0300

    Auth-api

--- src/api/auth-api.js
+++ src/api/auth-api.js
@@ -5,17 +5,6 @@ import * as Facebook from 'expo-facebook';
 import uuid from 'uuid';
 
 
-firebase.initializeApp({
-      apiKey: "AIzaSyCgomg2cFIcBKeWYzvzEt_RRXACA7Ow6hI",
-    authDomain: "android-b0813.firebaseapp.com",
-    databaseURL: "https://android-b0813-default-rtdb.firebaseio.com",
-    projectId: "android-b0813",
-    storageBucket: "android-b0813.appspot.com",
-    messagingSenderId: "246058769113",
-    appId: "1:246058769113:web:38de67b45bbe16d503fcc8",
-    measurementId: "G-E7GB2RTY0J",
-    facebookAppId: "470526873952355",
-    });
 export const logoutUser = () => {
   firebase.auth().signOut()
 }
